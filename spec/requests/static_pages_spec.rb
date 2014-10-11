07039443aee5ebf0065f2f906472ad9958d8ea17
Michael Haendler <Michael.Haendler@gmail.com>
2014-10-11 05:10:07 -0400

    right before I tried to get links like about_path working

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
- 
+
 describe "Static pages" do
 
   describe "Home page" do
@@ -18,7 +18,6 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).not_to have_title('| Home')
     end
-
   end
 
   describe "Help page" do
@@ -32,7 +31,6 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
-
   end
 
   describe "About page" do
@@ -46,7 +44,18 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
-
   end
 
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
