aefbee749d7e7cb554fdde2aa300df2a97e4ffb9
Michael Haendler <Michael.Haendler@gmail.com>
2014-10-20 21:26:21 -0400

    end of 6.2.3

---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+#all of contents of the contents of this file were added by me
+#and were taken from the demo_app project. 
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
