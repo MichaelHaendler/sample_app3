03110f24b339d5d1a262a794646068a7ef6a0f6d
Michael Haendler <Michael.Haendler@gmail.com>
2014-10-26 21:38:00 -0400

    all changes have been made, but the testing code isn't working correctly

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,66 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User2", email: "user3@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
+#1
   test "should be valid" do
     assert @user.valid?
   end
+
+/*
+#2
+ test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+ end
+
+#3
+   test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+#4
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+#5
+  test "email should not be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
+
+#6
+	test "email validation should accept valid addresses" do
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+		                     first.last@foo.jp alice+bob@baz.cn]
+		valid_addresses.each do |valid_address|
+		  @user.email = valid_address
+		  assert @user.valid?, "#{valid_address.inspect} should be valid"
+		end
+	end
+
+#7
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+#8
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+ */
+
 end
