81a3a01c1432b8d63ebb8cf9502a2d777a508733
Michael Haendler <Michael.Haendler@gmail.com>
2014-11-13 20:45:40 -0500

    now with working cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,8 +20,8 @@ module SessionsHelper
 
   def remember(user)
     user.remember
-    cookies.permanent.signed[:user_id]
-    cookies.permanent[:remember_token]
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def current_user
