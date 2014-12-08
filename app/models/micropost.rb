9856324d5859429b5ad313880fc8021ee1922ac3
Michael Haendler <Michael.Haendler@gmail.com>
2014-12-08 12:28:53 -0500

    end of chapter 11

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,15 @@ class Micropost < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
   default_scope -> { order(created_at: :desc)}
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  	def picture_size
+  		if picture.size > 5.megabytes
+  			errors.add(:pictures, "should be less than 5 megabytes")
+  		end
+  	end
+  	
 end
