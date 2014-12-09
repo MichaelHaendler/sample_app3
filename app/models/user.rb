4b69c368e7554091c0070e56a65327493c0b6bce
Michael Haendler <Michael.Haendler@gmail.com>
2014-12-09 18:01:47 -0500

    right before the start of the 12.2 section. all tests running properly

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
                                   dependent:   :destroy
 
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -100,7 +105,7 @@ class User < ActiveRecord::Base
     following.include?(other_user)
   end
 
-  
+
 
   private
 
