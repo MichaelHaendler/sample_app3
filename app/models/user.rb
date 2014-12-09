310b55f5e134a2ca4125d82b8e56b1c3947b374d
Michael Haendler <Michael.Haendler@gmail.com>
2014-12-09 17:57:48 -0500

    right before the start of the 12.1.5 section. all tests running

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,11 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
   has_many :microposts, dependent: :destroy
-    has_many :active_relationships, class_name:  "Relationship",
+  has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -81,6 +83,24 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where("user_id = ?" , id)
   end
+
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  
 
   private
 
