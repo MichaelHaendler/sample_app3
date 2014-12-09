f5644b8a9bc3c8c6d214de869b368d3534a3b697
Michael Haendler <Michael.Haendler@gmail.com>
2014-12-09 18:31:38 -0500

    testing in listing 12.29 went just fine. Everything is working except for that follow/unfollow button, which is giving me an error in regards to uninitialized constant relationshipsController error when clicked

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# User.create!(
-# 			  name: "Example User",
-# 			  email: "example@railstutorial.org",
-# 			  password: "foobar",
-# 			  password_confirmation: "foobar",
-# 			  admin: true,
-# 			  activated: true,
-# 			  activated_at: Time.zone.now
-# 			  )
+User.create!(
+			  name: "Example User",
+			  email: "example@railstutorial.org",
+			  password: "foobar",
+			  password_confirmation: "foobar",
+			  admin: true,
+			  activated: true,
+			  activated_at: Time.zone.now
+			  )
 
-# 99.times do |n|
-# 	name = Faker::Name.name
-# 	email = "example-#{n + 1}@railstutorial.org"
-# 	password = "password"
-# 	User.create!(
-# 				  name: name,
-# 				  email: email,
-# 				  password: password,
-# 				  password_confirmation: password,
-# 				  activated: true,
-# 				  activated_at: Time.zone.now
-# 				)
+99.times do |n|
+	name = Faker::Name.name
+	email = "example-#{n + 1}@railstutorial.org"
+	password = "password"
+	User.create!(
+				  name: name,
+				  email: email,
+				  password: password,
+				  password_confirmation: password,
+				  activated: true,
+				  activated_at: Time.zone.now
+				)
 
-# end
+end
 
 users = User.order(:created_at).take(6)
 
@@ -39,6 +39,12 @@ users = User.order(:created_at).take(6)
 	users.each { |user| user.microposts.create!(content: content)}
 end
 
-
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
 
